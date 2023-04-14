202e0812b40f7186290345328390e7f0d15867d5
shock-dev <shock.work@mail.ru>
2023-04-14 15:40:12 +0300

    mock events

--- src/resources/events.ts
+++ src/resources/events.ts
@@ -3,25 +3,28 @@ import { CalendarEvent, EventType } from '../types/event'
 export const eventTypes: EventType[] = ['event', 'task', 'reminder']
 
 export const mockEvents: CalendarEvent[] = [
-  { title: 'Великая среда', type: 'event', id: 1, date: new Date() },
-  { title: 'Щас бы пивка', type: 'event', id: 2, date: new Date() },
-  { title: 'Вчера', type: 'task', id: 3, date: new Date(2023, 3, 11) },
   {
-    title: 'Напоминание на завтра',
-    type: 'reminder',
-    id: 4,
-    date: new Date(2023, 3, 13),
-  },
-  {
-    title: 'Православная Пасха',
+    title: 'Orthodox Easter',
     type: 'holiday',
-    id: 5,
+    id: 1,
     date: new Date(2023, 3, 16),
   },
   {
-    title: 'Праздник Весны и Труда',
-    type: 'holiday',
-    id: 6,
-    date: new Date(2023, 4, 1),
+    title: 'Great Saturday',
+    type: 'reminder',
+    id: 2,
+    date: new Date(2023, 3, 15),
+  },
+  {
+    title: 'World Webmasters',
+    type: 'event',
+    id: 3,
+    date: new Date(2023, 3, 4),
+  },
+  {
+    title: 'Submit a project',
+    type: 'task',
+    id: 4,
+    date: new Date(2023, 3, 19),
   },
 ]
